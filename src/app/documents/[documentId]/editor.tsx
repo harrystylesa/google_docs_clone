8eb0254faaac93af3dd1f8980530a203d9a11327
meichuhe <1532744860@qq.com>
2025-07-16 23:05:05 +0200

    add tiptap extensions

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,6 +2,14 @@
 
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import ImageResize from "tiptap-extension-resize-image";
 
 const Editor = () => {
     const editor = useEditor({
@@ -11,8 +19,20 @@ const Editor = () => {
                 class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
             },
         },
-        extensions: [StarterKit],
-        content: '<p>Hello World! 🌎️</p>',
+        extensions: [
+            StarterKit,
+            Table,
+            TableCell,
+            TableHeader,
+            TableRow,
+            TaskItem.configure({
+                nested: true,
+            }),
+            TaskList,
+            Image,
+            ImageResize,
+        ],
+        content: `<img src="https://source.unsplash.com/8xznAGy4HcY/800x400" />`,
         // Don't render immediately on the server to avoid SSR issues
         immediatelyRender: false,
     })
