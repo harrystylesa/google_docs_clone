3ef87095b01fb66463ff78c24451c9b18c9ac413
meichuhe <1532744860@qq.com>
2025-07-31 20:35:10 +0200

    add convex

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import Link from "next/link";
 import { Navbar } from "./navbar";
 import { TemplatesGallery } from "./templates-gallery";
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 
 
 const Home = () => {
+  const documents = useQuery(api.documents.get)
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -11,6 +17,9 @@ const Home = () => {
       </div >
       <div className="mt-16">
         <TemplatesGallery />
+        {documents?.map((document) => (
+          <span>{document.title}</span>
+        ))}
       </div>
     </div>
   );
