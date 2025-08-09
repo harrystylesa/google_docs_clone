bfca9580b115f2e4abdb368b07d9c8830a661c55
meichuhe <1532744860@qq.com>
2025-08-09 15:58:37 +0200

    add margin function

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -21,14 +21,19 @@ import TextAlign from '@tiptap/extension-text-align'
 import { FontSizeExtension } from '@/extensions/font-size';
 import { LineHeightExtension } from '@/extensions/line-height';
 import { Ruler } from './ruler';
+import { useStorage } from '@liveblocks/react';
 
 import { useLiveblocksExtension } from "@liveblocks/react-tiptap";
 import { Threads } from './threads';
+
+import { LEFT_MARGIN_DEFAULT, RIGHT_MARGIN_DEFAULT } from '@/constants/margins';
 
 
 
 
 const Editor = () => {
+    const leftMargin = useStorage((root) => root.leftMargin) ?? LEFT_MARGIN_DEFAULT;
+    const rightMargin = useStorage((root) => root.rightMargin) ?? RIGHT_MARGIN_DEFAULT;
     const liveblocks = useLiveblocksExtension();
     const { setEditor } = useEditorStore();
     const editor = useEditor({
@@ -58,7 +63,7 @@ const Editor = () => {
         },
         editorProps: {
             attributes: {
-                style: `padding-left: 56px; padding-right: 56px;`,
+                style: `padding-left: ${leftMargin ?? 56}px; padding-right: ${rightMargin ?? 56}px;`,
                 class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
             },
         },
@@ -105,7 +110,7 @@ const Editor = () => {
             <Ruler />
             <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
                 <EditorContent editor={editor} />
-                <Threads editor={editor}/>
+                <Threads editor={editor} />
             </div>
         </div>
     )
